afd4965298bf2372f69c723ced4f0ab711e4af90
Spencer Hartland <spencerhartland@icloud.com>
2023-07-05 16:42:18 -0700

    Initial Commit

---
+++ Vinyls/VinylsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylsApp.swift
+//  Vinyls
+//
+//  Created by Spencer Hartland on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct VinylsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
